c818f8d270c697e67fd83f74b17f02fe65ef785e
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2022-09-30 16:22:30 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,31 @@
-function serpentine_x (x: number) {
-    if (x % 2 == 0) {
-        let matrixbreite = 0
-        x = matrixbreite - (1 - x)
+input.onButtonPressed(Button.A, function () {
+    strip.clear()
+})
+function serpentine (number: number) {
+    if (number % 2 == 0) {
+        x = matrixbreite + (1 - number)
+    } else {
+    	
     }
 }
-let y = 0
 let x = 0
-let strip = neopixel.create(DigitalPin.P0, 184, NeoPixelMode.RGB)
-pins.setMatrixWidth(DigitalPin.P0, 8)
+let strip: neopixel.Strip = null
+let matrixbreite = 0
+matrixbreite = 8
+strip = neopixel.create(DigitalPin.P0, 368, NeoPixelMode.RGB)
+pins.setMatrixWidth(DigitalPin.P0, matrixbreite)
+x = 0
+let y = 0
+strip.clear()
 strip.setBrightness(30)
 strip.setMatrixWidth(8)
 basic.forever(function () {
-    x = 0
-    y = 0
-    serpentine_x(x)
-    strip.setMatrixColor(x, y, neopixel.colors(NeoPixelColors.Red))
+    if (x >= matrixbreite) {
+        y += 1
+        x = 0
+    }
+    strip.setMatrixColor(serpentine(x), y, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    x += 1
+    basic.pause(100)
 })
